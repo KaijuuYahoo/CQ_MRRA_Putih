1a35aee8f167b47ca98e5d4ec2bd65666c90c3a1
KaijuuYahoo <udinaqimudin@gmail.com>
2026-03-13 14:56:23 +0700

    Menambahkan fungsi btnHitungMK_Click pada button Hitung MK dan menmabhkan logic Menghitung Jumlah Mata Kuliah

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -52,5 +52,24 @@ namespace PraktikumADO
                 MessageBox.Show(ex.Message);
             }
         }
+
+        private void btnHitungMK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+                string query = "SELECT COUNT(*) FROM MataKuliah";
+                cmd = new SqlCommand(query, conn);
+                int Jumlah = (int)cmd.ExecuteScalar();
+                txtHasil.Text = Jumlah.ToString();
+                MessageBox.Show("Koneksi ke Database Berhasil");
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
